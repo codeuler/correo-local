36b7f26639906ffa490e28619e68153f7d4b9cab
codeuler <159579558+codeuler@users.noreply.github.com>
2024-07-02 12:49:49 -0500

    Comenzar proyecto email local con Spring boot
    Las dependencias iniciales son:
    * Spring web
    * PostgreSQL Driver
    * Spring Data JPA
    * Thymeleaf

---
+++ src/test/java/com/example/codemail/CodemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
