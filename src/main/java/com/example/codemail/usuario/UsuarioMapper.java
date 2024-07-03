7afef83f3d33abb7194169aa035f83499ee793f5
codeuler <159579558+codeuler@users.noreply.github.com>
2024-07-03 14:27:26 -0500

    [backend] feat: Crear mapper para usuario
    Se crea la clase UsuarioMapper que permite convertir de un Objeto Usuario a UsuarioDto

---
+++ src/main/java/com/example/codemail/usuario/UsuarioMapper.java
@@ -0,0 +1,26 @@
+package com.example.codemail.usuario;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioMapper {
+
+    public Usuario toUsuario(UsuarioDto usuarioDto) {
+        return new Usuario(
+                usuarioDto.nombre(),
+                usuarioDto.apellido(),
+                usuarioDto.correo(),
+                usuarioDto.password()
+        );
+    }
+
+    public UsuarioDto usuarioDto(Usuario usuario) {
+        return new UsuarioDto(
+                usuario.getNombre(),
+                usuario.getApellido(),
+                usuario.getEmail(),
+                usuario.getPassword()
+        );
+    }
+
+}
