51a438f564c22ec8b2f602f3afb675c279b95085
codeuler <159579558+codeuler@users.noreply.github.com>
2024-07-03 18:58:23 -0500

    [backend] feat: Retornar razón del error
    Cuando se trata de crear un nuevo usuario a través de la API y el usuario no cumple
    con los estandarés de los campos, este arrojará un error especificando el porque es
    una mala petición

--- src/main/java/com/example/codemail/usuario/UsuarioControlador.java
+++ src/main/java/com/example/codemail/usuario/UsuarioControlador.java
@@ -1,9 +1,16 @@
 package com.example.codemail.usuario;
 
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 public class UsuarioControlador {
@@ -21,4 +28,18 @@ public class UsuarioControlador {
 
         return usuarioService.create(usuarioDto);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex
+    ) {
+        var errors = new HashMap<String,String>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            var fieldName = ((FieldError) error).getField();
+            var errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
 }
