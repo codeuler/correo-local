b83d0dfe6a12e2b04bbf46ce326d0b6aa7dd7e1c
codeuler <159579558+codeuler@users.noreply.github.com>
2024-07-03 14:34:28 -0500

    [backend] feat: crear clase para guardar Usuarios
    Se crea la clase UsuarioService con el fin de que a través de ella se pueda guardar un usuario en la base de datos

---
+++ src/main/java/com/example/codemail/usuario/UsuarioService.java
@@ -0,0 +1,18 @@
+package com.example.codemail.usuario;
+
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioMapper usuarioMapper;
+
+    public UsuarioService(UsuarioRepository usuarioRepository, UsuarioMapper usuarioMapper) {
+        this.usuarioRepository = usuarioRepository;
+        this.usuarioMapper = usuarioMapper;
+    }
+
+    public UsuarioDto create(UsuarioDto usuarioDto) {
+        var usuario = usuarioMapper.toUsuario(usuarioDto);
+        usuarioRepository.save(usuario);
+        return usuarioDto;
+    }
+
+}
