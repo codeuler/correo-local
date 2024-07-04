a0e0af171d4713ae4e02e072a2bd1bcc40935087
codeuler <159579558+codeuler@users.noreply.github.com>
2024-07-03 20:04:10 -0500

    [backend] feat: Agregar especificación de error
    Se agrega un detalle de cada uno de los errores de validación que puedan ocurrir cuando se trata
    de ejecutar un metodo post que busca guardar en la base de datos un nuevo usuario, con el fin de
    que la API sea más amigable con el consumidor final.

--- src/main/java/com/example/codemail/usuario/UsuarioDto.java
+++ src/main/java/com/example/codemail/usuario/UsuarioDto.java
@@ -3,18 +3,34 @@ package com.example.codemail.usuario;
 import jakarta.validation.constraints.*;
 
 public record UsuarioDto(
-        @NotBlank
-        @Size(min = 2, max = 15)
+        @NotBlank(
+                message = "El nombre no debe estar vacío"
+        )
+        @Size(
+                min = 2,
+                max = 15,
+                message = "El nombre debe tener como mínimo 2 letras y como máximo 15"
+        )
         String nombre,
-        @NotBlank
-        @Size(min = 2, max = 15)
+        @NotBlank(
+                message = "El nombre no debe estar vacío"
+        )
+        @Size(
+                min = 2,
+                max = 15,
+                message = "El nombre debe tener como mínimo 2 letras y como máximo 15"
+        )
         String apellido,
         @Email(
-                regexp = "^[a-zA-Z0-9]{5,30}@learncode.local$"
+                regexp = "^[a-zA-Z0-9]{5,30}@learncode.local$",
+                message = "El nombre del usuario solo puede contener letras y/o números, con un tamaño de entre 5 y 30 " +
+                        "caracteres, a demás se debe agregar el arroba junto con el dominio \"@learncode.local\""
         )
         String correo,
         @Pattern(
-                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,30}$"
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,30}$",
+                message = "La password debe tener una longitud de entre 8 y 30 caracteres, a demás, poseer como mínimo " +
+                        "cada uno de los siguientes parametros: minuscula, mayuscula, numero y caracter especial"
         )
         String password
 ) {
