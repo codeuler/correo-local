0f2d34ef348188eb82ec49e7762b6ce751c4a560
codeuler <159579558+codeuler@users.noreply.github.com>
2024-07-07 15:48:22 -0500

    [backend] feat: agregar endpoint registro y login
    Dentro del controlador de Auth se agregan los endpoint tanto get (plantillas html)
    como post.

--- src/main/java/com/example/codemail/Auth/AuthController.java
+++ src/main/java/com/example/codemail/Auth/AuthController.java
@@ -1,13 +1,42 @@
 package com.example.codemail.Auth;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @GetMapping("/registro")
     public String registroTemplate() {
         return "registro/registro";
     }
+
+    @GetMapping("/login")
+    public String loginTemplate() {
+        return "login/login";
+    }
+
+    @PostMapping(value = "login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody LoginRequest request
+    ) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping(value = "registro")
+    public ResponseEntity<AuthResponse> registro(
+            @RequestBody LoginRequest request
+    ) {
+        return ResponseEntity.ok(authService.registro(request));
+    }
+
 }
---
+++ src/main/java/com/example/codemail/Auth/AuthService.java
@@ -0,0 +1,14 @@
+package com.example.codemail.Auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    public AuthResponse login(LoginRequest request) {
+        return null;
+    }
+
+    public AuthResponse registro(LoginRequest request) {
+        return null;
+    }
+}
