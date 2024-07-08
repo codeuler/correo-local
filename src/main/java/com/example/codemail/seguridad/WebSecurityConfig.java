f69f641e85ab2283dc6ec3f8fd7e9706363c059a
codeuler <159579558+codeuler@users.noreply.github.com>
2024-07-08 18:54:24 -0500

    [backend] feat: modificar el filtro chain
    Se agrega el provedor de autenticacion por el creado propio, a demás se
    especifica que el filtro debe ir antes de verificar el usuario y contraseña

--- src/main/java/com/example/codemail/seguridad/WebSecurityConfig.java
+++ src/main/java/com/example/codemail/seguridad/WebSecurityConfig.java
@@ -1,31 +1,56 @@
 package com.example.codemail.seguridad;
 
+import com.example.codemail.Jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    public WebSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
 
     @Bean
+    /*
+     * En Spring Security, SecurityFilterChain representa una cadena de filtros de seguridad que se aplica a las
+     * solicitudes HTTP para proporcionar seguridad a nivel de aplicación, es decir, el metodo crear un filtro
+     * personalizado para las solicitudes http
+     */
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.
                 authorizeHttpRequests(
                     (requests) -> requests
                             .requestMatchers("/registro","/registro/**","/usuarios").permitAll()
                             .anyRequest().authenticated()
-        ).formLogin(
-                Customizer.withDefaults()
-        ).csrf(
-                AbstractHttpConfigurer::disable
-        );
+                )//Deshabilitar las sesiones
+                .sessionManagement( sessionManger ->
+                        sessionManger.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //Definir nuestro propio proveedor de autenticación
+                .authenticationProvider(authenticationProvider)
+                /*
+                 * Al llamar a addFilterBefore, estás indicando que jwtAuthenticationFilter debe ser agregado antes del
+                 * filtro de autenticación de nombre de usuario y contraseña (UsernamePasswordAuthenticationFilter).
+                 */
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(
+                    AbstractHttpConfigurer::disable
+                )
+        ;
         return http.build();
     }
 
