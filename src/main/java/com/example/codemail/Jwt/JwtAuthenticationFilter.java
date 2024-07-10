caf4f664306ae05937ca8b9a7e4e5d24e36e7421
codeuler <159579558+codeuler@users.noreply.github.com>
2024-07-10 15:14:07 -0500

    [backend] refactor: eliminar importaciones no usadas

--- src/main/java/com/example/codemail/Jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/codemail/Jwt/JwtAuthenticationFilter.java
@@ -14,7 +14,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.net.http.HttpHeaders;
 
 /**
  * Filtro personalizado para JWt
