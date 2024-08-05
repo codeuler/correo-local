d15f18c75a194012a547f3544c6634dcee4a42e6
codeuler <159579558+codeuler@users.noreply.github.com>
2024-08-05 12:29:11 -0500

    [backend] refactor: Eliminar endpoint innecesario
    El controlador del usuario, poseía un endpoint para crear un nuevo usuario,
    sin embargo, ese endpoint no estaba siendo usado, puesto que el usuario
    se crea desde otro endpoint.

--- src/main/java/com/example/codemail/usuario/UsuarioControlador.java
+++ src/main/java/com/example/codemail/usuario/UsuarioControlador.java
@@ -1,6 +1,5 @@
 package com.example.codemail.usuario;
 
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,14 +9,6 @@ public class UsuarioControlador {
 
     public UsuarioControlador(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
-    }
-
-    @PostMapping("/usuarios")
-    public UsuarioDto crearUsuario (
-            @Valid @RequestBody UsuarioDto usuarioDto
-    ) {
-
-        return usuarioService.create(usuarioDto);
     }
 
 }
